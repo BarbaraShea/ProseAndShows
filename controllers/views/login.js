c242afe920ab092891bc33049a3a17fe2c582001
Taylor Lilley <lilleyt09@gmail.com>
2021-05-12 18:45:24 -0400

    fixed routes

---
+++ controllers/views/login.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
